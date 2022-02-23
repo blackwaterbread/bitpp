3395a4bb744b44b98209580b65f6ce7c6b73a39c
blackwaterbread <blackwaterbread@gmail.com>
2022-02-24 04:27:06 +0900

    로그에 시각 추가

--- bitpp-server/src/lib/log.ts
+++ bitpp-server/src/lib/log.ts
@@ -1,19 +1,25 @@
+import { DateTime } from 'luxon';
+
+function now() {
+    return DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}
+
 export function logVerbose(msg: string) {
     console.log(`${msg}`);
 }
 
 export function logNormal(msg: string) {
-    console.log(`[Log] ${msg}`);
+    console.log(`[Log][${now()}]: ${msg}`);
 }
 
 export function logError(msg: string) {
-    console.error(`[Error] ${msg}`);
+    console.error(`${now()} | [Error] ${msg}`);
 }
 
 export function logWarning(msg: string) {
-    console.warn(`[Warning] ${msg}`);
+    console.warn(`${now()} | [Warning] ${msg}`);
 }
 
 export function logRequest(msg: string) {
-    console.log(`[Request] ${msg}`);
+    console.log(`${now()} | [Request] ${msg}`);
 }
